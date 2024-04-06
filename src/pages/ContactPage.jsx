2e142a946e6c3f4f19ee536f626a08a9663992dd
Nithish1018 <nithishkumar.sileveri123@gmail.com>
2024-04-06 14:25:46 +0530

    Minor Adjustments

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -29,27 +29,27 @@ const ContactPage = () => {
         <section className="text-black body-font relative">
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-12">
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Me</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Drop Your Queries And Messages From Here.</p>
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 font-extrabold">Contact Me</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base font-bold">Drop Your Queries And Messages From Here.</p>
                 </div>
                 <div className="lg:w-1/2 md:w-2/3 mx-auto">
                     <form onSubmit={handleSubmit}>
                         <div className="flex flex-wrap -m-2">
                             <div className="p-2 w-1/2">
                                 <div className="relative">
-                                    <label htmlFor="from_name" className="leading-7 text-sm text-black">Name</label>
+                                    <label htmlFor="from_name" className="leading-7 text-sm text-black font-bold">Name</label>
                                     <input type="text" id="from_name" name="from_name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
-                                    <label htmlFor="email" className="leading-7 text-sm text-black">Email</label>
+                                    <label htmlFor="email" className="leading-7 text-sm text-black font-bold">Email</label>
                                     <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-full">
                                 <div className="relative">
-                                    <label htmlFor="message" className="leading-7 text-sm text-black">Message</label>
+                                    <label htmlFor="message" className="leading-7 text-sm text-black font-bold">Message</label>
                                     <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                 </div>
                             </div>
@@ -60,8 +60,8 @@ const ContactPage = () => {
                     </form>
                     <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                         <a href='mailto: authorsampreeth@gmail.com' target="_top" className="text-white font-semibold">authorsampreeth@gmail.com</a>
-                        <p className="leading-normal my-5">Hyderabad
-                            <br />India
+                        <p className="leading-normal my-5 font-semibold">Hyderabad
+                            <br className='font-semibold' />India
                         </p>
                         <span className="inline-flex">
                             <a className="text-white font-semibold" href='https://m.facebook.com/sampreet.nani' target='_blank'>
